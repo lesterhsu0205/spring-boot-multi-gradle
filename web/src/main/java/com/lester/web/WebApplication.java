cc2377decff9457680128781885dbadf40da9fbd
lesterhsu <lesterhsu0205@hotmail.com>
2017-12-25 15:39:23 +0800

    新增設定

--- web/src/main/java/com/lester/web/WebApplication.java
+++ web/src/main/java/com/lester/web/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.lester.*")
+@ComponentScan("com.lester")
 public class WebApplication {
 
     public static void main(String[] args) {
